9e9daba855aa9953288bc8506e4e5d60164724f6
Kash <castillo.sergio@gmail.com>
2020-01-21 16:58:25 +0100

    Primer ejercicio

---
+++ Kash.Parallel.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+
+namespace Kash.Parallel.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Start Execution!!!");
+            //Using Thread without parameter
+            CreateThreadUsingThreadClassWithoutParameter();
+            System.Console.WriteLine("Finish Execution");
+            System.Console.ReadLine();
+        }
+        private static void CreateThreadUsingThreadClassWithoutParameter()
+        {
+            Thread thread;
+            thread = new Thread(new ThreadStart(PrintNumber10Times));
+            thread.Start();
+        }
+        private static void PrintNumber10Times()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.Write(1);
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
