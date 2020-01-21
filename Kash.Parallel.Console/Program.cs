b1c5bc69623d837b22654f7d89c8274894cd91f7
Kash <castillo.sergio@gmail.com>
2020-01-21 17:13:51 +0100

    Segunda prueba

--- Kash.Parallel.Console/Program.cs
+++ Kash.Parallel.Console/Program.cs
@@ -1,30 +1,50 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
-namespace Kash.Parallel.Console
+namespace Kash.Parallel
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Start Execution!!!");
+            Console.WriteLine("Start Execution!!!");
             //Using Thread without parameter
-            CreateThreadUsingThreadClassWithoutParameter();
-            System.Console.WriteLine("Finish Execution");
-            System.Console.ReadLine();
+            //CreateThreadUsingThreadClassWithoutParameter();
+            //Using Thread with parameter
+            CreateThreadUsingThreadClassWithParameter();
+            Console.WriteLine("Finish Execution");
+            Console.ReadLine();
         }
+
         private static void CreateThreadUsingThreadClassWithoutParameter()
         {
-            Thread thread;
-            thread = new Thread(new ThreadStart(PrintNumber10Times));
+            var thread = new Thread(new ThreadStart(PrintNumber10Times));
             thread.Start();
         }
+
+        private static void CreateThreadUsingThreadClassWithParameter()
+        {
+            var thread = new Thread(new ParameterizedThreadStart(PrintNumberNTimes));
+            thread.Start(100);
+        }
+
         private static void PrintNumber10Times()
         {
             for (int i = 0; i < 10; i++)
             {
-                System.Console.Write(i);
+                Console.Write(i);
             }
-            System.Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void PrintNumberNTimes(object times)
+        {
+            int n = Convert.ToInt32(times);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
         }
     }
 }
