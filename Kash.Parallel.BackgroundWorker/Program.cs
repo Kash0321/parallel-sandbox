58ed7c2c54e61892d607760508a5b1e3c7325d06
kash <sergio.castillo@pronet-ise.com>
2020-01-21 22:08:07 +0100

    BGW sin compilar

---
+++ Kash.Parallel.BackgroundWorker/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Kash.Parallel.BackgroundWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var backgroundWorker = new BackgroundWorker();
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.DoWork += SimulateServiceCall;
+            backgroundWorker.ProgressChanged += ProgressChanged;
+            backgroundWorker.RunWorkerCompleted +=
+            RunWorkerCompleted;
+            backgroundWorker.RunWorkerAsync();
+            Console.WriteLine("To Cancel Worker Thread Press C.");
+            while (backgroundWorker.IsBusy)
+            {
+                if (Console.ReadKey(true).KeyChar == 'C')
+                {
+                    backgroundWorker.CancelAsync();
+                }
+            }
+        }
+
+        // This method executes when the background worker finishes
+        // execution
+        private static void RunWorkerCompleted(object sender,
+        RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                Console.WriteLine(e.Error.Message);
+            }
+            else
+                Console.WriteLine($"Result from service call is {e.Result}");
+        }
+
+        // This method is called when background worker want to
+        // report progress to caller
+        private static void ProgressChanged(object sender,
+        ProgressChangedEventArgs e)
+        {
+            Console.WriteLine($"{e.ProgressPercentage}% completed");
+        }
+        // Service call we are trying to simulate
+        private static void SimulateServiceCall(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            StringBuilder data = new StringBuilder();
+            //Simulate a streaming service call which gets data and
+            //store it to return back to caller
+            for (int i = 1; i <= 100; i++)
+            {
+                //worker.CancellationPending will be true if user
+                //press C
+                if (!worker.CancellationPending)
+                {
+                    data.Append(i);
+                    worker.ReportProgress(i);
+                    Thread.Sleep(100);
+                    //Try to uncomment and throw error
+                    //throw new Exception("Some Error has occurred");
+                }
+                else
+                {
+                    //Cancels the execution of worker
+                    worker.CancelAsync();
+                }
+            }
+            e.Result = data;
+        }
+    }
+}
