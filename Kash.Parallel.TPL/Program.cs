f917e441a4ab04478bf33ea42f8e180baa9faf4c
kash <sergio.castillo@pronet-ise.com>
2020-01-22 21:52:57 +0100

    TPL sólo con Task

---
+++ Kash.Parallel.TPL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kash.Parallel.TPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Task(() => {
+                PrintNumberNTimes(100);
+            });
+            
+            Console.WriteLine("Inicio");
+            task.Start();
+            Console.WriteLine("Fin");
+
+            task.Wait();
+        }
+
+        private static void PrintNumberNTimes(object times)
+        {
+            int n = Convert.ToInt32(times);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
